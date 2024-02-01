f2582fda5ff0d241be76f67ca18725b2229da4d7
Tadiya Brown <tadiyabrown2000@gmail.com>
2024-02-01 18:05:07 -0500

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Tadiya Brown on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
